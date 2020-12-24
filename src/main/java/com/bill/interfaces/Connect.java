08fd63ebdcb1664e67c069601bc2b0bfbb05a8b7
Bill-Mwangi <56475136+bill-mwangi@users.noreply.github.com>
2020-12-24 22:51:37 +0300

    connect

--- src/main/java/com/bill/interfaces/Connect.java
+++ src/main/java/com/bill/interfaces/Connect.java
@@ -1,7 +1,7 @@
 package com.bill.interfaces;
 
 public interface Connect {
-    String URL = "jdbc:mysql://root@localhost/inventory";
-    String USERNAME = "root@localhost";
-    String PASSWORD = "Landscaper77";
+    String URL = "jdbc:mysql://invent@localhost/inventory";
+    String USERNAME = "invent@localhost";
+    String PASSWORD = "InventoryP4$$";
 }
